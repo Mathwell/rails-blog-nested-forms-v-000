01bb3e09148d294c170bc942cf8d60eb2c128fb8
Olena Ageyeva <eageeva@hotmail.com>
2018-01-26 22:32:19 +0000

    automatically backed up by learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -25,7 +25,11 @@ class PostsController < ApplicationController
   # POST /posts
   # POST /posts.json
   def create
-    @post = Post.new(post_params)
+    if !post_params[:tag_attributes][:name].present?
+      post_paramsp[:tag_attributes].delete
+    end
+        @post = Post.new(post_params)
+
     respond_to do |format|
       if @post.save
         format.html { redirect_to @post, notice: 'Post was successfully created.' }
